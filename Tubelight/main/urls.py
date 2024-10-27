c12ccb1f3094a0bb0b570b580518c7170ba6ef64
Aryan R <aryan.rajalbandi@gmail.com>
2024-10-28 02:02:09 +0530

    Finish events
    - finish events
    - finish create subevent

--- Tubelight/main/urls.py
+++ Tubelight/main/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("base/", views.base, name="base"),
     path("tubes/", views.display_events, name="display_events"),
     path("tubes/create/", views.create_event, name="create_event"),
+    path("tubes/connections/<int:event_id>/", views.display_subevents, name="display_subevents"),
+    path("tubes/connections/create/<int:event_id>/", views.create_subevent, name="create_subevent"),
 ]
--- Tubelight/main/views.py
+++ Tubelight/main/views.py
@@ -13,6 +13,34 @@ def base(request):
     return render(request, "base.html")
 
 
+def get_event_event_id(events_file, event_id):
+    with open(events_file, "r", newline="") as f:    
+        reader = csv.DictReader(f)
+        for row in reader:
+            if int(row["id"]) == event_id:
+                return row
+    return None
+
+
+def get_subevents_event_id(subevents_file, event_id):
+    subevents = []
+    with open(subevents_file, "r", newline="") as f:    
+        reader = csv.DictReader(f)
+        for row in reader:
+            if int(row["event_id"]) == event_id:
+                subevents.append(row)
+    return subevents
+
+
+def get_subevent_subevent_id(subevents_file, subevent_id):
+    with open(subevents_file, "r", newline="") as f:    
+        reader = csv.DictReader(f)
+        for row in reader:
+            if int(row["id"]) == subevent_id:
+                return row
+    return None
+
+
 def display_events(request):
     if request.session["is_authenticated"] == True:
         events = []
@@ -87,3 +115,57 @@ def create_event(request):
         return redirect("display_events")
     else:
         return render(request, "main/create_event.html")
+    
+
+def display_subevents(request, event_id):
+    subevents = []
+
+    events_file = "./database/events.csv"
+    subevents_file = "./database/subevents.csv"
+
+    event = get_event_event_id(events_file, event_id)
+
+    with open(subevents_file, "r", newline="") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            if int(row["event_id"]) == event_id:
+                subevents.append(row)
+    
+    data = {
+        "event": event,
+        "subevents": subevents,
+    }
+
+    return render(request, "main/display_subevents.html", data)
+
+
+def create_subevent(request, event_id):
+    if request.method == "POST":
+        name = request.POST["name"]
+        description = request.POST["description"]
+        duration = request.POST["duration"]
+
+        subevents_file_id = None
+        subevents_file = "./database/subevents.csv"
+        with open(subevents_file, "r", newline="") as f:
+            reader = list(csv.DictReader(f))
+            if len(reader):
+                last_row = reader[-1]
+                subevents_file_id = int(last_row["id"]) + 1
+            else:
+                subevents_file_id = 0
+
+        with open(subevents_file, "a", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow([subevents_file_id, event_id, name, description, duration])
+
+        return redirect("create_subevent", event_id=event_id)
+    else:
+        events_file = "./database/events.csv"
+        subevents_file = "./database/subevents.csv"
+        data = {
+            "event": get_event_event_id(events_file, event_id),
+            "subevents": get_subevents_event_id(subevents_file, event_id),
+        }
+
+        return render(request, "main/create_subevent.html", data)
